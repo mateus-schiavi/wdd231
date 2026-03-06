c7b42bb7860b8428d252fb93c6738f652be23883
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2026-03-06 12:23:57 -0300

    done

---
+++ script/script.js
@@ -0,0 +1,7 @@
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const temperature = document.getElementById("temperature");
+const description = document.getElementById("description");
+
+temperature.textContent = "79°F"
+description.textContent = "Partly Cloudy in Suzano"
