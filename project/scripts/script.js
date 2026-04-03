eeb9de696da9c6f4514b97d9255a61c921c99282
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2026-04-02 21:04:27 -0300

    project site plan added

---
+++ project/scripts/script.js
@@ -0,0 +1,7 @@
+// Current Year
+const year = document.querySelector('#year');
+year.textContent = new Date().getFullYear();
+
+// Last Modified
+const lastModified = document.querySelector('#lastModified');
+lastModified.textContent = document.lastModified;
